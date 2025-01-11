bbeec1a8b496f6d9bf3c1aadb7e114e9f8e64747
Sakshi Bhandari <bhandarisakshi234@gmail.com>
2025-01-11 17:22:04 +0530

    Add newly created Matrix Files

---
+++ MatrixDiagonalSum.cpp
@@ -0,0 +1,95 @@
+# include <iostream>
+using namespace std;
+
+class Matrix
+{
+    private:
+    int **Arr = NULL;
+    int Rows = 0, Cols = 0;
+
+    public:
+    Matrix(int Row, int Col);
+    ~Matrix();
+    void Accept();
+    void DiagonalSum();
+
+};
+
+Matrix :: Matrix(int Row, int Col)
+{
+    Rows = Row;
+    Cols = Col;
+
+    Arr = new int*[Row];
+
+    for (int i = 0; i < Row; i++)
+    {
+        Arr[i] = new int[Col];
+    }
+    
+}
+
+Matrix :: ~Matrix()
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        delete []Arr[i];
+    }
+    delete []Arr;
+}
+
+void Matrix :: Accept()
+{
+    cout << "Enter the elements: " << endl;
+
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            cin >> Arr[i][j];
+        }
+        cout << endl;
+    }
+    
+}
+
+void Matrix :: DiagonalSum()
+{
+    int iSum = 0;
+
+    if (Rows != Cols)
+    {
+        cout << "Number of rows and columns shouls be equal" << endl;
+    }
+    
+
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            if(i == j)
+            {
+               iSum += Arr[i][j];
+            }
+        }
+    }
+    cout << "Sum of diagonal elements: " << iSum << endl;
+}
+int main()
+{
+    int iRow = 0, iCol = 0;
+
+    cout << "Enter the number of rows: " << endl;
+    cin >> iRow;
+
+    cout << "Enter the number of column: " << endl;
+    cin >> iCol;
+
+    Matrix obj = Matrix(iRow, iCol);
+
+    obj.Accept();
+    obj.DiagonalSum();
+
+
+    return 0;
+}
