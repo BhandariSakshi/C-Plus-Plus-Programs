d800a30bc1acae3d4bb1d1e39c8bc5f26133e8c9
Sakshi Bhandari <bhandarisakshi234@gmail.com>
2026-01-20 10:32:07 +0530

    New File

---
+++ Pattern46.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+class Pattern
+{
+public:
+    void Display(int iRow, int iCol);
+};
+
+void Pattern ::Display(int iRow, int iCol)
+{
+    if (iRow != iCol)
+    {
+        cout << "Number of rows and column is not same" << endl;
+        return;
+    }
+
+     for (int i = 1; i <= iRow; i++)
+    {
+        for (int k = iRow - i; k > 0; k--)
+        {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = iRow - 1; i >= 1; i--)
+    {
+        for (int k = iRow - i; k > 0; k--)
+        {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout << "Enter the number of row: " << endl;
+    cin >> iValue1;
+
+    cout << "Enter the number of column: " << endl;
+    cin >> iValue2;
+
+    Pattern *obj = new Pattern();
+
+    obj->Display(iValue1, iValue2);
+
+    return 0;
+}
